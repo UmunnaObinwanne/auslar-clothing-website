2d8ef97a217c7ef1ded9200f32f75be32e0742a3
Mike <broadwaymarketingconsults@gmail.com>
2024-04-16 16:18:34 +0100

    adding starting website directory code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import Directory from "./components/directory";
+
+function App() {
+  return (
+    <>
+      <Directory />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/category.jsx
@@ -0,0 +1,14 @@
+export default function Category({ title, imageUrl }) {
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+}
